f75fb742d7b6e48603ef05f6a0451238d1f42a20
tzortzis <tzo@gm.com>
2020-10-02 16:18:20 +0300

    added some stuff

--- fabric-samples/multi-node-swarm/generateChannelTx.sh
+++ fabric-samples/multi-node-swarm/generateChannelTx.sh
@@ -5,11 +5,11 @@ export FABRIC_CFG_PATH=$PWD
 
 mkdir channel-artifacts
 
-../bin/configtxgen -profile SampleMultiNodeEtcdRaft -outputBlock ./channel-artifacts/genesis.block
+../bin/configtxgen -profile OrdererGenesis -outputBlock ./channel-artifacts/genesis.block
 
-../bin/configtxgen -profile TwoOrgsChannel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID mychannel
+../bin/configtxgen -profile ChannelConfig -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID mychannel
 
-../bin/configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org1MSPanchors.tx -channelID mychannel -asOrg Org1MSP
+../bin/configtxgen -profile ChannelConfig -outputAnchorPeersUpdate ./channel-artifacts/Org1MSPanchors.tx -channelID mychannel -asOrg Org1MSP
 
-../bin/configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org2MSPanchors.tx -channelID mychannel -asOrg Org2MSP
+../bin/configtxgen -profile ChannelConfig -outputAnchorPeersUpdate ./channel-artifacts/Org2MSPanchors.tx -channelID mychannel -asOrg Org2MSP
 
