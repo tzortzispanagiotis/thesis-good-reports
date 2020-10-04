1d9dc42c6fec5036376124f24c26f1c0daa253e9
tzortzispanagiotis <tzortzispanagiotis@gmail.com>
2020-10-04 17:56:09 +0300

    modified env file

--- fabric-samples/test-docker-stack/env.sh
+++ fabric-samples/test-docker-stack/env.sh
@@ -3,4 +3,4 @@
 export FABRIC_VERSION=1.4.8;
 export SYS_CHANNEL=byfn-sys-channel;
 
-docker stack deploy --compose-file docker-compose-stack.yaml up;
+docker stack deploy --compose-file docker-compose-stack.yaml fabric;
