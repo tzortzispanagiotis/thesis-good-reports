aafa88f99ff72758522ea3769dd226111a95d186
tzortzispanagiotis <tzortzispanagiotis@gmail.com>
2020-10-08 15:05:24 +0300

    2org2peer

--- fabric-samples/test-docker-stack/createChannel.sh
+++ fabric-samples/test-docker-stack/createChannel.sh
@@ -12,7 +12,7 @@ echo "Joining peer0.org1 to channel..."
 docker exec $CLI peer channel join -b mychannel.block
 
 echo "Joining peer1.org1 to channel..."
-docker exec -e CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/users/Admin@org1.example.com/msp -e CORE_PEER_ADDRESS=peer1.org1.example.com:7051 -e CORE_PEER_LOCALMSPID="Org1MSP" -e CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/ca.crt $CLI peer channel join -b mychannel.block
+docker exec -e CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp -e CORE_PEER_ADDRESS=peer1.org1.example.com:7051 -e CORE_PEER_LOCALMSPID="Org1MSP" -e CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/ca.crt $CLI peer channel join -b mychannel.block
 
 
 echo "Joining peer0.org2 to channel..."
